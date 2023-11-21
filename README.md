f52633f19d41e1c31b5258ccbb93bcf6339ed183
dominussnake1 <d.anagnostopoulos02@gmail.com>
2023-11-21 16:36:42 +0200

    .

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
+## System Information
+
 OS: Ubuntu 22.04.3 LTS<br>
 GPU: AMD® Radeon RX 6900 XT<br>
 CPU: Intel® Core™ i7-13700K<br>
 RAM: 32 GB DDR5 <br>
 
+## Install the required packages.
 ```bash
 pip install -r requirements.txt
 ```
 
+## This is required for the Project to work.
 ```bash
 git clone https://github.com/Farama-Foundation/ViZDoom.git
 ```
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+from vizdoom import *
+import random
+import time
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    game = DoomGame()
+    game.load_config('ViZDoom/scenarios/basic.cfg')
+    game.init()
 
+    actions = np.identity(3, dtype=np.uint8)
+
+    episodes = 10
+    for episode in range(episodes):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = game.get_state()
+            img = state.screen_buffer
+            info = state.game_variables
+            reward = game.make_action(random.choice(actions))
+            print("Reward:", reward)
+            time.sleep(0.02)
+        print("Result:", game.get_total_reward())
+        time.sleep(2)
